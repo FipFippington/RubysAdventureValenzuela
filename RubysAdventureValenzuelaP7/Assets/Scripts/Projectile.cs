d1392089f81ad6838dea81a08f696e3286124067
FipFippington <116121197+FipFippington@users.noreply.github.com>
2022-12-13 21:35:40 -0800

    Almost done fixing my mistakes
    I fixed like half of them

--- RubysAdventureValenzuelaP7/Assets/Scripts/Projectile.cs
+++ RubysAdventureValenzuelaP7/Assets/Scripts/Projectile.cs
@@ -4,38 +4,28 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Launch(Vector2 direction, float force)
     {
-        rigidbody2D.AddForce(direction * force);
+        rigidbody2d.AddForce(direction * force);
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Projectile Collision with" + other.gameObject);
         Destroy(gameObject);
-    }
-
-    void Launch()
-    {
-        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2D.position + vector2.up * 0.5f, Quaternion.identity);
-
-        Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(LookDirection, 300);
-
-        animator.SetTrigger("Launch");
     }
 }
